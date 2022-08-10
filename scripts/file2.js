8eb34a2a96b35d3fefc2e2417409648b428f6119
jirawittn <jirawitth@kkumail.com>
2022-08-10 21:42:56 +0700

    master

--- scripts/file2.js
+++ scripts/file2.js
@@ -1,5 +1,5 @@
 
-var positive = [];
+var list = [];
 
 //loop to check number > 0 and no decimal point and number < 0 and no decimal point
 function readInput() {
@@ -7,36 +7,33 @@ function readInput() {
         let message = prompt('Enter an integer (a negative integer to quit):');
         var number = Number(message);
         if (number>=0 && !(number % 1 != 0) ) {
-            positive.push(number);
+            list.push(number);
             continue loop;
         } else if (number<0 && !(number % 1 != 0) ) {
             break loop;
         }
-    } 
+    } return list;
 }
 
 //function average,minimum,maximum
 function average() {
-    const avg = positive.reduce((a, b) => a + b, 0) / positive.length;
+    const avg = list.reduce((a, b) => a + b, 0) / list.length;
     return avg;
 }
 
 function minimum() {
-    const min = Math.min.apply(Math,positive);
+    const min = Math.min.apply(Math,list);
     return min;
 }
 
 function maximum() {
-    const max = Math.max.apply(Math,positive);
+    const max = Math.max.apply(Math,list);
     return max;
 }
 
 function displayStats(list) {
-    average(list);
-    minimum(list);
-    maximum(list);
-    (positive.length > 0)? 
-    alert('For the list '+ positive + ' the average is ' + average(list).toFixed(2) + ' the minimum is ' + minimum(list) + ' and the maximum is ' + maximum(list)):
+    (list.length > 0)? 
+    alert('For the list '+ list + ' the average is ' + average(list).toFixed(2) + ' the minimum is ' + minimum(list) + ' and the maximum is ' + maximum(list)):
     alert('For the list is empty,'+ ' the average is 0' + ' the minimum is 0' + ' and the maximum is 0');
     // if (positive.length > 0) {
     //     alert('For the list '+ positive + ' the average is ' + average(list).toFixed(2) + ' the minimum is ' + minimum(list) + ' and the maximum is ' + maximum(list) )
